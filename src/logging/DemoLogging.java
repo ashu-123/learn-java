f6b63eba1eb79990cd7450859a81e6dcdd1d044a
Ashutosh <Ashutosh>
2024-03-10 16:05:38 +0530

    Configuring log properties and initializing them using the log Manager

--- src/logging/DemoLogging.java
+++ src/logging/DemoLogging.java
@@ -10,18 +10,21 @@ public class DemoLogging {
 
     private static final Logger LOGGER = Logger.getLogger(DemoLogging.class.getName());
 
-    static {
-        FileHandler fileHandler = null;
-        try {
-            fileHandler = new FileHandler(DemoLogging.class.getSimpleName() + ".log");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        fileHandler.setLevel(INFO);
-        LOGGER.addHandler(fileHandler);
-    }
+//    static {
+//        FileHandler fileHandler = null;
+//        try {
+//            fileHandler = new FileHandler(DemoLogging.class.getSimpleName() + ".log");
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        fileHandler.setLevel(INFO);
+//        LOGGER.addHandler(fileHandler);
+//    }
 
     public static void main(String[] args) throws IOException {
+
+        LoggingUtils.initLogManger();
+
         LOGGER.log(INFO, "Initiating main()");
     }
 }
