8619126a18beaef1f884c6cd2ee78bb029c6ce80
Ashutosh <Ashutosh>
2024-03-12 07:23:16 +0530

    Using ReadWrite Lock to avoid race conditions in the cache HashMap

--- src/concurrent_patterns/CacheWithReadWriteLock.java
+++ src/concurrent_patterns/CacheWithReadWriteLock.java
@@ -8,17 +8,34 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class CacheWithReadWriteLock {
 
     private Map<Long, String> cache = new HashMap<>();
 
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+    private Lock readLock = lock.readLock();
+    private Lock writeLock = lock.writeLock();
+
     public String get(Long key) {
-        return cache.get(key);
+        readLock.lock();
+        try {
+            return cache.get(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public String put(Long key, String value) {
-        return cache.put(key, value);
+        writeLock.lock();
+        try {
+            return cache.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public static void main(String[] args) {
@@ -34,7 +51,7 @@ public class CacheWithReadWriteLock {
                 while (true) {
                     long key = random.nextInt(1_000);
                     cacheWithReadWriteLock.put(key, Long.toString(key));
-                    if(cacheWithReadWriteLock.get(key)==null) {
+                    if (cacheWithReadWriteLock.get(key) == null) {
                         System.out.println("Key " + key + "has not been put in the map");
                     }
                 }
@@ -44,12 +61,12 @@ public class CacheWithReadWriteLock {
 
         ExecutorService executorService = Executors.newFixedThreadPool(4);
 
+        System.out.println("Adding values ..");
         try {
-            for (int i=0;i<4;i++) {
+            for (int i = 0; i < 4; i++) {
                 executorService.submit(new Producer());
             }
-        }
-        finally {
+        } finally {
             executorService.shutdown();
         }
     }
