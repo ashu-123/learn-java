9e7be0ee5fb5be8a675541931ef171342eebcde1
Ashutosh <Ashutosh>
2024-04-01 13:27:21 +0530

    Reading back ints from a gzip compressed file

--- src/io/ReadingBytes.java
+++ src/io/ReadingBytes.java
@@ -6,12 +6,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class ReadingBytes {
 
     public static void main(String[] args) {
         String fileName = "src/resources/ints.bin";
-        Path pathName = Paths.get(fileName);
+        String fileNameZip = "src/resources/ints.bin.gz";
+        Path pathName = Paths.get(fileNameZip);
         try {
             long size = Files.size(pathName);
             System.out.println(pathName + " size " + size);
@@ -20,14 +22,19 @@ public class ReadingBytes {
         }
 
         List<Integer> ints = new ArrayList<>();
-        try(InputStream inputStream = new FileInputStream(new File(fileName));
-        DataInputStream dataInputStream = new DataInputStream(inputStream)) {
+        try (InputStream inputStream = new FileInputStream(fileNameZip);
+             GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+             DataInputStream dataInputStream = new DataInputStream(gzipInputStream)) {
             while (true) {
                 ints.add(dataInputStream.readInt());
             }
 
+        } catch (EOFException eofException) {
+            ints.forEach(System.out::println);
+            System.out.println(ints.size());
+            eofException.printStackTrace();
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
         }
-        catch (EOFException eofException) { ints.forEach(System.out::println);System.out.println(ints.size());eofException.printStackTrace(); }
-        catch (IOException ioException) { ioException.printStackTrace(); }
     }
 }
