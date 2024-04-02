59026d6190381b3f84ea3c99d50c774c246b9d68
Ashutosh <Ashutosh>
2024-04-02 13:24:00 +0530

    Deserializing person object using proxy resolver

--- src/io/ReadingObjects.java
+++ src/io/ReadingObjects.java
@@ -12,20 +12,32 @@ public class ReadingObjects {
         String fileName = "src/resources/person.bin";
         File file = new File(fileName);
 
-        try(InputStream inputStream = new FileInputStream(file);
-            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
-            System.out.println(objectInputStream.readObject());
-            System.out.println(objectInputStream.readObject());
+//        try(InputStream inputStream = new FileInputStream(file);
+//            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
 //            System.out.println(objectInputStream.readObject());
-        }
-        catch (IOException | ClassNotFoundException ioException) {
-            ioException.printStackTrace();
-        }
+//            System.out.println(objectInputStream.readObject());
+////            System.out.println(objectInputStream.readObject());
+//        }
+//        catch (IOException | ClassNotFoundException ioException) {
+//            ioException.printStackTrace();
+//        }
 
         String fileNameExternalize = "src/resources/person-externalize.bin";
         File fileExternalize = new File(fileNameExternalize);
 
         try(InputStream inputStream = new FileInputStream(fileExternalize);
+            ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
+            System.out.println(objectInputStream.readObject());
+            System.out.println(objectInputStream.readObject());
+        }
+        catch (IOException | ClassNotFoundException ioException) {
+            ioException.printStackTrace();
+        }
+
+        String fileNameProxy = "src/resources/person-proxy.bin";
+        File fileProxy = new File(fileNameProxy);
+
+        try(InputStream inputStream = new FileInputStream(fileProxy);
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)){
             System.out.println(objectInputStream.readObject());
             System.out.println(objectInputStream.readObject());
--- src/io/model/PersonProxy.java
+++ src/io/model/PersonProxy.java
@@ -2,11 +2,19 @@ package io.model;
 
 import java.io.DataOutputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamException;
 import java.io.Serializable;
 
 public class PersonProxy implements Serializable {
 
     String name;
+
+    private Object readResolve() throws ObjectStreamException {
+        String[] fields = this.name.split(":-:");
+        var name = fields[0];
+        var age = Integer.parseInt(fields[1]);
+        return new Person(name, age);
+    }
 
     PersonProxy() { }
 
