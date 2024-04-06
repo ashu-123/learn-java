dbbb0d93641cad60dad9e546061774e948d2a139
Ashutosh <Ashutosh>
2024-04-06 15:29:12 +0530

    Realizing the methods, fields and modifiers of a class using the Reflection API

---
+++ src/reflection/model/Person.java
@@ -0,0 +1,42 @@
+package reflection.model;
+
+public class Person {
+
+    String name;
+    int age;
+
+    public Person() { }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static Person of(String name, int age) {
+        return new Person(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
